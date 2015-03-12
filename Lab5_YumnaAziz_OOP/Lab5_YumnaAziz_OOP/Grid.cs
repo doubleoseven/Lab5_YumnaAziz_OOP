8a0b03453f08dc7a81bd16992393b0e65dd3db22
Yumna Aziz <yumna_ansari@hotmail.com>
2015-03-12 10:26:03 +0300

    Tried making the rows and columns of the gird

--- Lab5_YumnaAziz_OOP/Lab5_YumnaAziz_OOP/Grid.cs
+++ Lab5_YumnaAziz_OOP/Lab5_YumnaAziz_OOP/Grid.cs
@@ -7,15 +7,22 @@ namespace Lab5_YumnaAziz_OOP
 {
     class Grid
     {
-        //Tile[,] tile = new Tile[10, 10];
+        Tile[,] tile = new Tile[10, 10];
         int[,] tiles = new int[10, 10];
         int width = 60;
         int height = 60;
+        Game1 game = new Game1();
 
         public void Load(int[,] grid)
         {
             tiles = grid;
-            
+            for (int i = 0; i<10; i++)
+            {
+                for (int j = 0; j<10; j++)
+                {
+                    grid[i][j]
+                }
+            }
         }
 
 
